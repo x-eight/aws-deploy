8284a2dd748eb3a4d55b3b181b9c98a2a86f0809
x-eight <saul9412@hotmail.com>
2023-03-18 18:38:10 -0500

    change status 2

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import { STATUS_CODES } from "http";
+
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const port = (process.env.PORT || 5000) as number;
 const variable = process.env.VARIABLE;
 
@@ -17,7 +21,7 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/test", (req, res) => {
-  res.status(204).send('Application is up and running!');
+  res.status(200).send({status:'running'});
 });
 
 // start the Express server
