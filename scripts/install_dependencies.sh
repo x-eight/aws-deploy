8e54714d0b71051b2925169b6aeb550db543232e
x-eight <saul9412@hotmail.com>
2024-01-09 11:42:32 -0500

    test 1

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,6 +5,7 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install -y yarn
 
+sudo apt install nodejs
 
 # install pm2 module globaly
 npm install -g pm2
