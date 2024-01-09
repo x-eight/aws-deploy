df198857b91169b3ea0140f478a4c60d3ffbe4a1
x-eight <saul9412@hotmail.com>
2024-01-08 22:27:54 -0500

    yarn 2

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,7 +3,7 @@
 # Instalación de Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update && sudo apt install yarn
+sudo apt update && sudo apt install -y yarn
 
 
 #cd /var/www/aws-deploy
