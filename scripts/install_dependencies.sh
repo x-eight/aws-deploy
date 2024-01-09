7dbd0edf46943967933055fa8b99815d009097d9
x-eight <saul9412@hotmail.com>
2024-01-09 11:57:37 -0500

    test 4

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,9 +5,9 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install -y yarn
 
-sudo apt install nodejs
-sudo apt install -y npm
+#sudo apt install nodejs
+#sudo apt install -y npm
 
 # install pm2 module globaly
-npm install -g -y pm2
-pm2 update
+#npm install -g -y pm2
+#pm2 update
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,6 +4,4 @@ cd /var/aws-deploy
 yarn install
 
 # Inicia el servidor en segundo plano
-#yarn dev
-
-pm2 start src/index.ts
+yarn dev
