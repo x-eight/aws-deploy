ba5a483e64010630c181c878e7f587c951106a73
x-eight <saul9412@hotmail.com>
2024-01-08 22:54:40 -0500

    yarn 6

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,4 +2,13 @@
 
 cd /var/www/aws-deploy
 yarn install
-yarn dev
+
+# Inicia el servidor en segundo plano
+yarn dev &
+
+# Espera hasta que el servidor esté disponible (ejemplo: espera 30 segundos)
+counter=0
+while [ $counter -lt 30 ] && ! nc -z localhost 5000; do
+    sleep 1
+    ((counter++))
+done
