3a8bc0ce71d0c3798231152575db13fe06e0f92d
x-eight <saul9412@hotmail.com>
2024-01-10 00:07:46 -0500

    test

--- src/index.ts
+++ src/index.ts
@@ -11,20 +11,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined'));
 const port = (process.env.PORT || 5000) as number;
 const variable = process.env.VARIABLE || "code_deploy";
+const other = process.env.OTHER || "github";
 console.log("test")
 
 app.get("/", (req, res) => {
-  res.send({ message: "autodeploy", port, variable });
+  res.send({ message: "autodeploy", port, variable,other });
 });
 
 app.get("/hello", (req, res) => {
   console.log("GET console")
-  res.send({ message: "hello autodeploy", port, variable });
+  res.send({ message: "hello autodeploy", port, variable,other });
 });
 
 app.post("/hello", (req, res) => {
   console.log("POST console")
-  res.send({ message: "hello autodeploy", port, variable });
+  res.send({ message: "hello autodeploy", port, variable,other });
 });
 
 app.get("/test", (req, res) => {
