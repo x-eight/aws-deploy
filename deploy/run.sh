38d72844b2b21a8f8d5fbab27d6524a84068596b
x-eight <saul9412@hotmail.com>
2024-01-10 01:01:52 -0500

    test

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 # Definir la ubicación del archivo .env
-ENV_FILE="/var/aws-deploy/.env"
+#ENV_FILE="/var/aws-deploy/.env"
 
 # Crear o sobrescribir el archivo .env con las variables de entorno actuales
-env | grep -E "^(VARIABLE=|OTHER=)" > "$ENV_FILE"
+#env | grep -E "^(VARIABLE=|OTHER=)" > "$ENV_FILE"
 
 # Asegurarse de que el archivo tenga permisos adecuados
-chmod 600 "$ENV_FILE"
+#chmod 600 "$ENV_FILE"
 
 #sudo cp "$HOME/variables/.env" /var/aws-deploy/.env
+sudo cp /etc/profile.d/myenvvars.sh /var/aws-deploy/.env
 
 cd /var/aws-deploy
 
