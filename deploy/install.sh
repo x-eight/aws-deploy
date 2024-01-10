4f54da06484cd80358d0f12c2286e0297e8be235
x-eight <saul9412@hotmail.com>
2024-01-10 09:47:20 -0500

    test

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,4 +1,4 @@
-# add nodejs to yum
+#!/usr/bin/env bash
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash -
 #yum install nodejs -y #default-jre ImageMagick
 sudo apt install nodejs
--- deploy/run.sh
+++ deploy/run.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
-
-# Definir la ubicación del archivo .env
-#ENV_FILE="/var/aws-deploy/.env"
-
-# Crear o sobrescribir el archivo .env con las variables de entorno actuales
-#env | grep -E "^(VARIABLE=|OTHER=)" > "$ENV_FILE"
-
-# Asegurarse de que el archivo tenga permisos adecuados
-#chmod 600 "$ENV_FILE"
-
-#sudo cp "$HOME/variables/.env" /var/aws-deploy/.env
 sudo cp /etc/profile.d/myenvvars.sh /var/aws-deploy/.env
-
 cd /var/aws-deploy
 
-#sudo cp "$HOME/variables/.env" .env
+
 
 npm install
 
