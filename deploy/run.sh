384c457c73a5d8698ee4804a0031610ca7c6a84e
x-eight <saul9412@hotmail.com>
2024-01-10 00:11:54 -0500

    test

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-sudo cp "$HOME/variables/.env" /var/aws-deploy/.env
+# Definir la ubicación del archivo .env
+ENV_FILE="/var/aws-deploy/.env"
+
+# Crear o sobrescribir el archivo .env con las variables de entorno actuales
+env | grep -E "^(VARIABLE=|OTHER=)" > "$ENV_FILE"
+
+# Asegurarse de que el archivo tenga permisos adecuados
+chmod 600 "$ENV_FILE"
+
+#sudo cp "$HOME/variables/.env" /var/aws-deploy/.env
 
 cd /var/aws-deploy
 
-sudo cp "$HOME/variables/.env" .env
+#sudo cp "$HOME/variables/.env" .env
 
 npm install
 
